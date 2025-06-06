3880149b1556fe8458a5d9fc071e73ad09773916
Seo-aron <dkfhs4337@naver.com>
2025-06-06 22:19:55 +0900

    회원 조회 기능

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,39 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 같은 이름의 회원은 없다고 가정
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(member1 -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    // 전체 회원 조횐
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    // 특정 회원 조회
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
